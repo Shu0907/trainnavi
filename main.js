e538dc0e3e989ae3583bc86ae5c26b4cd0b926d6
Shu0907 <b7770834shusei@gmail.com>
2025-12-01 19:35:11 +0900

    Refactor main.js to include Electron app setup

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-{
-  "name": "trainnavi",
-  "version": "1.0.0",
-  "main": "main.js",
-  "scripts": {
-    "start": "electron ."
-  }
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 650,
+    height: 320,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
 }
+
+app.whenReady().then(createWindow);
